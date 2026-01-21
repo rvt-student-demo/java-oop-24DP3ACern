61f95b3f1ecab41d2ab771d57d6bc2fccf1071f7
24DP3ACern <A240672AC@rvt.lv>
2026-01-21 10:36:41 +0200

    s

---
+++ src/main/java/rvt/teacher.java
@@ -0,0 +1,11 @@
+package rvt;
+
+public class teacher {
+    private String name;
+    private String adress;
+
+    public teacher(String name, String adress){
+        this.name = name;
+        this.adress = adress;
+    }
+}
